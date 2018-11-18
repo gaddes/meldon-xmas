37b5ea79eb3aa9cccc436c84130ad29fa7675198
Matthew Gaddes <matthew.gaddes@gmail.com>
2018-11-18 17:12:39 +0000

    Add date created to schema.

--- models/Store.js
+++ models/Store.js
@@ -7,15 +7,15 @@ const reminderSchema = new mongoose.Schema({
         type: String,
         trim: true, // Remove whitespace
         required: 'Please enter a reminder!'
-    }
+    },
     // description: {
     //     type: String,
     //     trim: true
     // },
-    // created: {
-    //     type: Date,
-    //     default: Date.now
-    // }
+    created: {
+        type: Date,
+        default: Date.now
+    }
 }, {
     // Make sure virtuals are visible when we take a pre= h.dump (by default they are not!)
     // Note we can still use the virtual fields without this step, but it's nice to see them for debugging (and dumping!)
